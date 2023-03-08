f1b6a7ffc3097311c0968af1f85b190113b99303
Natali <114168847+Natali9108@users.noreply.github.com>
2023-03-08 18:40:08 +0200

    виконано дз 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryBoxRef = document.querySelector('.gallery');
+const galleryCard = createGalleryCard(galleryItems);
+galleryBoxRef.insertAdjacentHTML('afterbegin', galleryCard);
+
+function createGalleryCard(galleryItems) {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item"
+	  href="${original}">
+	  <img class="gallery__image"
+	  src="${preview}" alt="${description}" />
+	</a>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
