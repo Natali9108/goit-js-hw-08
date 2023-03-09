91050555efc4ff0003ad78b3b57e47b8c0e12d98
Natali <114168847+Natali9108@users.noreply.github.com>
2023-03-09 20:51:41 +0200

    update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const handelPlay = function (evt) {
+  localStorage.setItem(STORAGE_KEY, evt.seconds);
+};
+
+player.on('timeupdate', _throttle(handelPlay, 1000));
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(currentTime);
