52557139c39124d37794c0e2b8d837424492a5ca
Natali <114168847+Natali9108@users.noreply.github.com>
2023-03-10 19:32:02 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,16 @@
 import _throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('.feedback-form input');
+const textareaRef = document.querySelector('.feedback-form textarea');
+
 const STORAGE_KEY = 'feedback-form-state';
 
 formRef.addEventListener('input', _throttle(handelFormInput, 500));
 formRef.addEventListener('submit', handelFormSubmit);
 
-let formData = {};
+const formData = {};
+getInputValue();
 
 function handelFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
@@ -18,12 +22,18 @@ function handelFormInput(evt) {
 
 function handelFormSubmit(evt) {
   evt.preventDefault();
-  const savedInputValue = localStorage.getItem(STORAGE_KEY);
 
-  if (savedInputValue) {
-    console.log(savedInputValue);
-    formData = savedInputValue;
-  }
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+}
+
+function getInputValue() {
+  const savedInputValue = localStorage.getItem(STORAGE_KEY);
+  const jsonParse = JSON.parse(savedInputValue);
+  console.log(inputRef.value);
+  if (savedInputValue) {
+    console.log(savedInputValue);
+    inputRef.value = jsonParse.email;
+    textareaRef.value = jsonParse.message;
+  }
 }
