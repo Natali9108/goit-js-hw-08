4a1c5c4c00f8b59a20b6f10dc407c505fa5200bc
Natali <114168847+Natali9108@users.noreply.github.com>
2023-03-10 18:43:12 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import _throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+formRef.addEventListener('input', _throttle(handelFormInput, 500));
+formRef.addEventListener('submit', handelFormSubmit);
+
+let formData = {};
+
+function handelFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  const value = JSON.stringify(formData);
+
+  localStorage.setItem(STORAGE_KEY, value);
+}
+
+function handelFormSubmit(evt) {
+  evt.preventDefault();
+  const savedInputValue = localStorage.getItem(STORAGE_KEY);
+
+  if (savedInputValue) {
+    console.log(savedInputValue);
+    formData = savedInputValue;
+  }
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
